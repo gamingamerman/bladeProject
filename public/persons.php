baf74f17e7875217f793876314258fd24423b308
gamingamerman <gamingamerman@gmail.com>
2023-01-16 21:07:27 +0100

    new changes

---
+++ public/persons.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once ('../src/connection.php');
+
+use Dsw\Blade\Person as Person;
+
+$persons = Person::all();
+
+foreach ($persons as $person) {
+    echo '<p>' . $person->name . '</p>';
+}
---
+++ src/Person.php
@@ -0,0 +1,8 @@
+<?php
+namespace Dsw\Blade;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Person extends Model {
+    protected $table = 'Person';
+}
